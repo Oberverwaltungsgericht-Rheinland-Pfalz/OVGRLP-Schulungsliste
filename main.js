37794e647de86ecd0eb7e48b0716bc62b4d69d0e
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-11-22 15:07:16 +0100

    Korrektur Türchen 15

---
+++ main.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<script src="vue.global.js"></script>
+<link href="styles.css" rel="stylesheet"></link>
+</head>
+<body>
+<div id="app"></div>
+
+<script src="main.js"></script>
+
+</body>
+</html>
