fccbf2cbb5a4d28350e6036f25021af640d9f342
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-11-26 13:16:55 +0100

    Optimierung für die Anzeige von Bildern und Videos

--- data/hinweise.js
+++ data/hinweise.js
@@ -2,6 +2,6 @@ export default {
 	title: "Informationssicherheit",
 	waitInfoHeader: "Zu früh",
 	waitInfo: "Auf den {clickedDay}. Dezember musst du noch etwas warten 😿",
-	copyrightMark: "Erstellt von den Mitarbeitern der IT-Sicherheit der Justiz mit Inhalten des LDI-RLP",
+	copyrightMark: "Erstellt von den Mitarbeitern der IT-Sicherheit der Justiz mit Texten des LDI-RLP und Videos des BSI",
 	dbKeyCheckDoors: "itsec2021"
 }
--- modal.js
+++ modal.js
@@ -1,4 +1,5 @@
-const checkMedia = /(.*\.mp4)|(.*\.ogg)|(.*\.webm)/g;
+const checkMedia = /(.*\.mp4)|(.*\.ogg)|(.*\.webm)/
+const checkImage = /(.*\.jp[e]?g)|(.*\.png)|(.*\.gif)|(.*\.svg)|(.*\.bmp)|(.*\.webp)/
 
 export default {
     name: 'modal',
@@ -9,13 +10,20 @@ export default {
 		}
 	},
 	computed: {
+		isVideo () {
+			return checkMedia.test(this.media)
+		},
+		isImage () {
+			return checkImage.test(this.media)
+		},
+
 	},
 	methods:{
-		isVideo () {
-			let r = checkMedia.test(this.media)
-			console.log("is video "+r + this.media)
-			return r
-		  },
+		styleModal () {
+			if (this.isVideo) return `height: fit-content;`
+			else if (this.isImage) return `width: fit-content; max-width: 100vw; max-height: fit-content;`
+			else return ''
+		},
 		escape(text){
   		  if(!text) return ""
 		  
@@ -29,16 +37,17 @@ export default {
      
     template: `
       <div class="modal-background" @click="close">
-	    <div class="modal" :class="{message: message}">
+	    <div class="modal" :class="{message: message}" :style="styleModal()">
 	      <div class="modal-header">
 		    <span>&#9447;</span>
 		    <h3>{{header}}</h3>
           </div>
 		  <div class="modal-content">
 		    <span v-if="text" v-html="escape(text)"></span>
-			<video v-if="media" :key="'videokey'+now" :class="'videokey'+now" controls="" autoplay="" name="media" style="height: 100%;width: 100%;">
-			  <source :src="'media/'+media" type="video/mp4">
+			<video v-if="isVideo" :key="'videokey'+now" :class="'videokey'+now" controls="" autoplay="" name="media" style="height: 100%;width: 100%;">
+			  <source :src="'media/' + media" type="video/mp4">
 			</video>
+			<img v-else-if="isImage" :src="'media/' + media" id="modal-img">
 		  </div>
 		</div>
       </div>	  
